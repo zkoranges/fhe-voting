f3b7885f7b93b5c7bace61a12a2e8787ca036b15
zkoranges <hangingoranges@proton.me>
2024-05-18 12:28:57 +0200

    feat: some layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -15,12 +15,20 @@ export default function LandingPage() {
 
   return (
     <>
-      <main className='flex min-h-screen flex-col items-center justify-center'>
+      <main className='fixed w-full flex justify-center p-10 bg-gradient-to-b'>
+        <div>
+          <h1 className='text-3xl font-bold py-5'>FHE Voting</h1>
+        </div>
+
+
+       {/* Footer */}
+        <div className='fixed bottom-0 w-full flex justify-center p-4 bg-gradient-to-b'>
+
         <div className='flex space-x-5'>
-          <p className='rounded-xl border border-slate-500 bg-gradient-to-b from-zinc-800/30 to-zinc-500/40 p-4 flex items-center'>
-            Voting Contract Address: {votingAddress}
+          <p className='rounded-xl border border-slate-500 bg-gradient-to-b from-zinc-800/30 to-zinc-500/40 p-4 flex items-center text-xs'>
+            Voting Contract : {votingAddress}
           </p>
-          <div className='rounded-xl border border-slate-500 bg-gradient-to-b from-zinc-800/30 to-zinc-500/40 p-4 flex flex-col'>
+          <div className='rounded-xl text-xs border border-slate-500 bg-gradient-to-b from-zinc-800/30 to-zinc-500/40 p-4 flex flex-col'>
             <p>Watching Blocks on {chainId ? chainId : 'Loading...'}</p>
             <div className='flex items-center justify-center space-x-2'>
               <div className='relative'>
@@ -30,12 +38,13 @@ export default function LandingPage() {
                   } opacity-75 animate-ping`}
                 ></div>
                 <div
-                  className={`relative h-2 w-2 rounded-full ${blockNumber.data ? 'bg-green-500' : 'bg-orange-500'}`}
+                  className={`relative h-2 w-2 rounded-full  ${blockNumber.data ? 'bg-green-500' : 'bg-orange-500'}`}
                 ></div>
               </div>
               <p>{blockNumber.data ? Number(blockNumber.data) : 'Loading...'}</p>
             </div>
           </div>
+        </div>
         </div>
       </main>
     </>
